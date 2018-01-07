b841b8e03ccb8c79d01dff3e284c6a40c5e81858
Jeffrey Wang <jqywang.95@gmail.com>
2018-01-07 10:46:23 -0800

    "Set up framework for the function"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,13 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var jsonString = '';
+  if (Array.isArray(obj)){
+  	//add brackets on each side, run stringify on each element of array
+  } else if (typeof obj === 'object'){
+  	//add curly brackets on each side, run stringify on each element
+  } else if (obj && typeof obj === 'string'){
+  	//add quotes on each side
+  }
+  return ''+ obj;
 };
