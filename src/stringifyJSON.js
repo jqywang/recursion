075d74408c66eefd7a0099a0e79cadbb4c03336b
Jeffrey Wang <jqywang.95@gmail.com>
2018-01-07 11:50:00 -0800

    "stringifyJSON works as intended"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -6,11 +6,19 @@
 var stringifyJSON = function(obj) {
   var jsonString = '';
   if (Array.isArray(obj)){
-  	//add brackets on each side, run stringify on each element of array
-  } else if (typeof obj === 'object'){
-  	//add curly brackets on each side, run stringify on each element
-  } else if (obj && typeof obj === 'string'){
-  	//add quotes on each side
+  	var arrayString = obj.map(function(obj){return stringifyJSON(obj)});
+  	return '[' + arrayString + ']'
+  } else if (obj && typeof obj === 'object'){
+  	var keyValue = [];
+  	for (var key in obj) {
+  		if(key !== undefined && obj[key] !== undefined && typeof obj[key] !== 'function'){
+  			keyValue.push('"'+ key + '"' + ':' + stringifyJSON(obj[key]));
+  		}
+  	}
+  	keyValue.join(',');
+  	return '{' + keyValue + '}';
+  } else if (typeof obj === 'string'){
+  	return '\"' + obj + '\"'
   }
-  return ''+ obj;
+  return obj + '';
 };
