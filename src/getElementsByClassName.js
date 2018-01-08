6f265e1b26c2b2046a58fc88a2bfb531f90d9862
Jeffrey Wang <jqywang.95@gmail.com>
2018-01-08 11:42:14 -0800

    "getElementsByClassName now works as intended"

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, element) {
+  var elementsArray = [];
+  if(element === undefined){
+  	element = document.body;
+  }
+	if(element.classList !== undefined){  
+	  if(element.classList.contains(className)){
+	  	elementsArray.push(element);
+	  }
+	}
+  for(var i = 0; i<element.childNodes.length; i++){
+  	var childrenList = getElementsByClassName(className, element.childNodes[i]);
+  	elementsArray = elementsArray.concat(childrenList);
+  }
+  return elementsArray;
 };
